275652e50f6dba279e76651285649dd205f1372c
Bratapfel2000 <43928387+Bratapfel2000@users.noreply.github.com>
2019-08-22 19:41:44 +0200

    Add files via upload
    Simple printer of 2 lines

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld {
+    public static void main(String[] args) {
+    // simple output
+        System.out.println("Hello, World!"); // first line.  println sends next print to next line
+        System.out.println("All good over there?"); // another line
+}
+}
