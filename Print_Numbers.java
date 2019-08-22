7978a816a53c1cb4dad33b86c9a545dfd2dd951d
Bratapfel2000 <43928387+Bratapfel2000@users.noreply.github.com>
2019-08-22 21:15:30 +0200

    Add files via upload

---
+++ Print_Numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Print_Numbers {
+
+    public static void main(String[] args) {
+        double x = 2;
+        int y = 3;
+        int z = 4;
+
+
+
+        System.out.printf("%.2f x, %d y, %d z\n",
+                          x, y, z);
+
+    }
+
+}
